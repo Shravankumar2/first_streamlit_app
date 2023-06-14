f20245d730cd1df7a735c9074546163f1b88727c
Shravan kumar <68984533+Shravankumar2@users.noreply.github.com>
2023-06-14 10:57:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit= 
+#add_my_fruit= 
 
 #add a second text box
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
